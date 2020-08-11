33445181e83580780549575b0ab0e220f889d9a3
stevenmcdonald <engineers2526@gmail.com>
2020-08-11 00:00:21 -0500

    implemented and tested the frontend of the signup page.

---
+++ frontend/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import { UserSignupPage } from './pages/UserSignupPage'
+import 'jquery/dist/jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as apiCalls from './api/apiCalls';
+
+const actions = {
+  postSignup: apiCalls.signup
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <UserSignupPage  actions={actions}/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
